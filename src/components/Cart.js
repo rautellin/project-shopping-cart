63a91635dec413c9f53d8ce007049f4859664f09
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-04-30 01:42:07 +0200

    done with the functionality

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,12 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { CartItem } from './CartItem'
 
 export const Cart = () => {
-  // TODO - fetch products from the cart store
-  const products = []
+  const products = useSelector((store) => store.cart.items)
 
-  // TODO - calculate total from the sum of all products in the cart
-  const totalPrice = 0
+  const totalPrice = useSelector((store) => store.cart.items.reduce((total, item) => (total + (item.price * item.quantity)), 0));
 
   return (
     <div className="cart">
--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -7,7 +7,21 @@ export const cart = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-      console.log(action)
+      const existingProduct = state.items.find((item) => item.id === action.payload.id)
+      if (existingProduct) {
+        existingProduct.quantity += 1
+      } else {
+        state.items.push({ ...action.payload, quantity: 1 })
+      }
+    },
+    removeItem: (state, action) => {
+      const existingProduct = state.items.find((item) => item.id === action.payload.id)
+
+      if (existingProduct && existingProduct.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== action.payload.id)
+      } else if (existingProduct) {
+        existingProduct.quantity -= 1
+      }
     }
   }
 })
